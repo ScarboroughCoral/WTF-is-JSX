35b6ac17afcb7579067f376e58398ff111de0672
Mingyue Li <3249977074@qq.com>
2021-09-25 20:58:09 +0800

    feat: add simple render

---
+++ index.ts
@@ -0,0 +1,29 @@
+namespace WTFISJSX {
+    export type Node = ElementNode | TextNode;
+    type ElementNode = {
+        nodeName: keyof HTMLElementTagNameMap,
+        attributes: {
+            [key: string]: string
+        },
+        children: Node[]
+    }
+    type TextNode = string;
+}
+
+function hyper(nodeName: keyof HTMLElementTagNameMap, attributes, ...args: WTFISJSX.Node[]): WTFISJSX.Node {
+    const children = args.length > 0 ? [...args] : null;
+    return { nodeName, attributes, children };
+}
+
+function render(vnode: WTFISJSX.Node) {
+    if (typeof vnode === 'string') return document.createTextNode(vnode);
+
+    const node = document.createElement(vnode.nodeName);
+
+    const { attributes = {}, children = [] } = vnode;
+    Object.entries(attributes).forEach(([key, value]) => {
+        node.setAttribute(key, value);
+    });
+    children.forEach(child => node.append(render(child)));
+    return node;
+}
